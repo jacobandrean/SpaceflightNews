0c6b5f54ea45132e6b46754468cf8efb49f91cd9
jacobandrean <andreanjacob99@gmail.com>
2024-10-01 12:34:52 +0700

    Initial Commit

---
+++ SpaceFlightNews/SpaceFlightNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFlightNewsApp.swift
+//  SpaceFlightNews
+//
+//  Created by Jacob Andrean on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceFlightNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
